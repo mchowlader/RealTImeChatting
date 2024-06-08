cebb4c0357ff4f47c398aa2177d20fce8a4f55c2
Mithun Chandra Howlader <mithun.howlader222@yahoo.com>
2024-06-09 00:53:08 +0600

    commit for realtime chatting project add in the github

---
+++ src/Chatting.Web/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+public class ChatHub : Hub
+{
+    private static readonly Dictionary<string, string> _userConnections = new();
+
+    public async Task SendMessage(string sender, string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", sender, user, message);
+    }
+}
